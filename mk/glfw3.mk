331508a9b409cbe1a8422068ced51f6fd8756ea1
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2021-02-28 14:32:59 +0100

    Makefile, glfw3.mk: a better make system

---
+++ mk/glfw3.mk
@@ -0,0 +1,44 @@
+# ======================================================================
+# glfw3.mk
+# ======================================================================
+
+NUKEDIR ?= ${HOME}/git/Nuklear
+FONT_NAME ?= Karla-Regular.ttf
+FONT_HEIGHT ?= 16
+NUKEFONT = $(NUKEDIR)/extra_font/$(FONT_NAME)
+# Flags
+CDEFINES := -D'NUKEFONT="$(NUKEFONT)"' -DFONT_HEIGHT=$(FONT_HEIGHT)
+CFLAGS += -std=c99 -Wall -pedantic -O2 -I$(NUKEDIR) $(CDEFINES)
+
+
+ifeq ($(OS),Windows_NT)
+	BIN := $(BIN).exe
+	LIBS = -lglfw3 -lopengl32 -lm -lGLU32 -lGLEW32
+else
+	UNAME_S := $(shell uname -s)
+	GLFW3 := $(shell pkg-config --libs glfw3)
+	ifeq ($(UNAME_S),Darwin)
+		LIBS := $(GLFW3) -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo -lm -lGLEW -L/usr/local/lib
+	else
+		LIBS = $(GLFW3) -lGL -lm -lGLU -lGLEW
+	endif
+endif
+
+all: $(DBIN) $(BIN)
+
+clean:
+	rm -f $(BIN) $(OBJS)
+
+run: all
+	./$(BIN)
+
+$(DBIN):
+	@mkdir -p $(DBIN)
+
+$(DBIN)/%.o:%.c common.h
+	$(CC) -c $< $(CFLAGS) -o $@
+
+$(BIN): $(OBJS)
+	$(CC) $(OBJS) -o $@ $(LIBS)
+
+.PHONY: all clean run
