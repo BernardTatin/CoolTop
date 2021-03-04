dbf2c07711237d6334cc9d3d914e89ff931ce7e0
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2021-03-04 11:30:19 +0100

    All static data have their own structure

--- CoolTop/static-system-data.h
+++ CoolTop/static-system-data.h
@@ -5,9 +5,13 @@
 #ifndef COOLTOP_STATIC_SYSTEM_DATA_H
 #define COOLTOP_STATIC_SYSTEM_DATA_H
 
-extern struct utsname unames;
-extern char str_processors[64];
-extern char str_libc_version[64];
+typedef struct {
+  struct utsname unames;
+  char str_processors[64];
+  char str_libc_version[64];
+} static_data;
+
+extern static_data global_static_data;
 
 nk_bool load_unames(void);
 
