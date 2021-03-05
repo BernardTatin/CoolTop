d3fd40c021cca7f7f61d9408bbd2527fa7b4ac98
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2021-03-05 11:31:27 +0100

    The about box bug resolved with a _behavior_ structure

--- CoolTop/main.c
+++ CoolTop/main.c
@@ -3,23 +3,6 @@
 #include "cooltop-common.h"
 
 /* =============================================================== */
-void show_about_box(struct nk_context *ctx) {
-  /* about popup */
-  static struct nk_rect s = {20, 100, 300, 190};
-  if (nk_popup_begin(ctx, NK_POPUP_STATIC, "About", NK_WINDOW_CLOSABLE, s)) {
-    nk_layout_row_dynamic(ctx, 20, 1);
-    nk_label(ctx, "CoolTop", NK_TEXT_CENTERED);
-    nk_label(ctx, "By Bernard Tatin", NK_TEXT_CENTERED);
-    nk_label(ctx, "With love", NK_TEXT_CENTERED);
-    nk_label(ctx, "and with nuklear", NK_TEXT_CENTERED);
-    nk_label(ctx, "which is licensed under", NK_TEXT_CENTERED);
-    nk_label(ctx, "the public domain License.", NK_TEXT_CENTERED);
-    nk_popup_end(ctx);
-  } else {
-    global_environment.show_app_about = nk_false;
-  }
-}
-
 /* ===============================================================
  *
  *                          CoolTop
@@ -93,8 +76,8 @@ int main(void) {
     }
     nk_glfw3_new_frame(&glfw);
     /* GUI */
-    new_sub_window(ctx, "Unames", nk_rect(5, 5, 430, 250), fill_unames);
-    new_sub_window(ctx, "Memory", nk_rect(440, 5, 430, 250), fill_memory);
+    new_sub_window(ctx, "Unames", nk_rect(5, 5, 430, 250), &unames_behavior);
+    new_sub_window(ctx, "Memory", nk_rect(440, 5, 430, 250), &memory_behavior);
 
     /* ----------------------------------------- */
 
