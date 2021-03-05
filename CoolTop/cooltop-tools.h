41ceaa7a5b6cd97d0eafd7cf28f2162bfe4ff316
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2021-03-05 11:31:27 +0100

    The about box bug resolved with a _behavior_ structure

--- CoolTop/cooltop-tools.h
+++ CoolTop/cooltop-tools.h
@@ -6,11 +6,16 @@
 #ifndef COOLTOP_COLTOP_TOOLS_H
 #define COOLTOP_COLTOP_TOOLS_H
 
+typedef struct {
+  nk_bool show_app_about;
+  nk_bool (*fill_me)(struct nk_context *ctx);
+} sub_window_behavior;
+
 nk_bool new_sub_window(struct nk_context *ctx,
                        const char* title,
                        struct nk_rect bounds,
-                       nk_bool (fill_me)(struct nk_context *ctx) );
-nk_bool fill_unames(struct nk_context *ctx);
-nk_bool fill_memory(struct nk_context *ctx);
+                       sub_window_behavior *window_behavior);
+extern sub_window_behavior unames_behavior;
+extern sub_window_behavior memory_behavior;
 
 #endif // COOLTOP_COLTOP_TOOLS_H
