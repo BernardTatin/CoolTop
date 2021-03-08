8558b7fc1e79415cfb7d28c4dec79c3f41e1e3a9
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2021-03-08 06:48:14 +0100

    closed #22: head of these files is quite good

--- include/environment.h
+++ include/environment.h
@@ -1,6 +1,33 @@
-//
+// ======================================================================
+// environment.h
 // Created by bernard on 28/02/2021.
-//
+// ======================================================================
+/*
+MIT License
+
+Copyright (c) 2021 Bernard Tatin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+// ======================================================================
+// Global environment management.
+// ======================================================================
 
 #ifndef COOLTOP_ENVIRONNEMENT_H
 #define COOLTOP_ENVIRONNEMENT_H
@@ -28,7 +55,7 @@ typedef struct {
   // global font and size
   char *font_name;
   float font_size;
-  // backgroud color
+  // background color
   struct nk_colorf bg;
 
 } AppConfiguration;
