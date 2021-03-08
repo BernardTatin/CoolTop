fc3d6cb43584b9dc9d95ba1b8de2474f9f718f8d
bernard (pop) tat1 <bernard.tatin@outlook.fr>
2021-03-08 01:08:00 +0100

    BROKEN CODE - great refactoring to use full of structures

--- CoolTop/main.c
+++ CoolTop/main.c
@@ -1,6 +1,7 @@
 /* nuklear - 1.32.0 - public domain */
 
 #include "cooltop-common.h"
+#include "environment.h"
 
 /* =============================================================== */
 /* ===============================================================
@@ -8,94 +9,46 @@
  *                          CoolTop
  *
  * ===============================================================*/
-static void error_callback(int e, const char *d) {
-  printf("Error %d: %s\n", e, d);
-}
-
 int main(void) {
   /* Platform */
-  struct nk_glfw glfw = {0};
-  static GLFWwindow *win;
-  int width = 0, height = 0;
-  struct nk_context *ctx;
-  struct nk_colorf bg;
-  static float last_t = 0.0;
+  static float last_t = 0.0f;
   float current_time;
 
 
-  init_environment(&global_environment);
-  /* GLFW */
-  glfwSetErrorCallback(error_callback);
-  if (!glfwInit()) {
-    fprintf(stdout, "[GFLW] failed to init!\n");
-    exit(1);
-  }
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-  win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CoolTop", NULL, NULL);
-  glfwMakeContextCurrent(win);
-  glfwGetWindowSize(win, &width, &height);
+  init_environment(&global_environment,
+                   "Cooltop",
+                   NULL);
+  init_application(&global_environment);
 
-  /* OpenGL */
-  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-  glewExperimental = 1;
-  if (glewInit() != GLEW_OK) {
-    fprintf(stderr, "Failed to setup GLEW\n");
-    exit(1);
-  }
+  global_environment.app_configuration.bg.r = 0.10f;
+  global_environment.app_configuration.bg.g = 0.18f;
+  global_environment.app_configuration.bg.b = 0.24f;
+  global_environment.app_configuration.bg.a = 1.0f;
   load_unames();
   load_dynamic_data();
   last_t = glfwGetTime();
-  ctx = nk_glfw3_init(&glfw, win, NK_GLFW3_INSTALL_CALLBACKS);
-  /* Load Fonts: if none of these are loaded a default font will be used  */
-  /* Load Cursor: if you uncomment cursor loading please hide the cursor */
-  {
-    struct nk_font_atlas *atlas;
-    nk_glfw3_font_stash_begin(&glfw, &atlas);
-#if defined(NUKEFONT) && defined(FONT_HEIGHT)
-    struct nk_font *nukefont =
-        nk_font_atlas_add_from_file(atlas, NUKEFONT, FONT_HEIGHT, 0);
-#endif
-    nk_glfw3_font_stash_end(&glfw);
-#if defined(NUKEFONT)
-    nk_style_set_font(ctx, &nukefont->handle);
-#endif
-  }
-
-  bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f;
-  while (!glfwWindowShouldClose(win) && !global_environment.ready_to_exit) {
-    glfwWaitEventsTimeout(global_environment.delta_t);
+  while (!glfwWindowShouldClose(global_environment.glfw_states.win) &&
+         !global_environment.app_states.ready_to_exit) {
+    glfwWaitEventsTimeout(global_environment.app_configuration.delta_t);
     current_time = glfwGetTime();
-    if (current_time - last_t >= global_environment.delta_t) {
+    if (current_time - last_t >= global_environment.app_configuration.delta_t) {
       load_dynamic_data();
       last_t = glfwGetTime();
     }
-    nk_glfw3_new_frame(&glfw);
+    nk_glfw3_new_frame(&global_environment.glfw_states.glfw);
     /* GUI */
-    new_sub_window(ctx, "Unames", nk_rect(5, 5, 430, 250), &unames_behavior);
-    new_sub_window(ctx, "Memory", nk_rect(440, 5, 430, 250), &memory_behavior);
+    new_sub_window(global_environment.nuklear_states.ctx,
+                   "Unames",
+                   nk_rect (5, 5, 430, 250),
+                   &unames_behavior);
+    new_sub_window(global_environment.nuklear_states.ctx,
+                   "Memory",
+                   nk_rect(440, 5, 430, 250),
+                   &memory_behavior);
 
     /* ----------------------------------------- */
-
-    /* Draw */
-    glfwGetWindowSize(win, &width, &height);
-    glViewport(0, 0, width, height);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(bg.r, bg.g, bg.b, bg.a);
-    /* IMPORTANT: `nk_glfw_render` modifies some global OpenGL state
-     * with blending, scissor, face culling, depth test and viewport and
-     * defaults everything back into a default state.
-     * Make sure to either a.) save and restore or b.) reset your own state
-     * after rendering the UI. */
-    nk_glfw3_render(&glfw, NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER,
-                    MAX_ELEMENT_BUFFER);
-    glfwSwapBuffers(win);
+    draw_and_render(&global_environment);
   }
-  nk_glfw3_shutdown(&glfw);
-  glfwTerminate();
+  application_terminate(&global_environment);
   return 0;
 }
